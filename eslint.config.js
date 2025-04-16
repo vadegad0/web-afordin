03c2e5bd7c59e28b911dedabed56200c382b37d0
Felix Icaza <fx.joliett17@gmail.com>
2025-04-15 19:07:01 -0600

    feat: install eslint-plugin-jsx-a11y and update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,19 @@ import love from 'eslint-config-love'
 import astro from 'eslint-plugin-astro'
 
 export default [
-  ...astro.configs.recommended,
+  ...astro.configs['flat/recommended'],
+  ...astro.configs['flat/jsx-a11y-recommended'],
   {
+    files: ['**/*.{js,mjs}', '**/*.ts'],
     ...love,
-    files: ['**/*.js', '**/*.ts'],
     rules: {
-      ...love.rules,
       '@typescript-eslint/unbound-method': 'off',
       '@typescript-eslint/no-magic-numbers': 'off',
       '@typescript-eslint/prefer-destructuring': 'off',
       '@typescript-eslint/triple-slash-reference': 'off',
       '@typescript-eslint/no-unsafe-type-assertion': 'off',
       '@typescript-eslint/no-unnecessary-type-parameters': 'off',
-      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
     },
   },
 ]
