3226f2c6c3426d4838b59bd35dcd7f3366619366
Felix Icaza <fx.joliett17@gmail.com>
2025-04-15 20:56:58 -0600

    fix: add ignores option in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import love from 'eslint-config-love'
 import astro from 'eslint-plugin-astro'
 
 export default [
+  { ignores: ['dist/'] },
   ...astro.configs['flat/recommended'],
   ...astro.configs['flat/jsx-a11y-recommended'],
   {
