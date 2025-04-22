3b53476574994361c9ac2408bb1ae2b1f832928b
developerleonardo <leo.11hnz@gmail.com>
2025-04-21 20:44:04 -0500

    feat: add tooltips and FooterDesignersContributors component and update footer to use it; fix contributor data structure

--- src/data/designersData.ts
+++ src/data/designersData.ts
@@ -1,12 +1,12 @@
 export const designersData = [
   {
-    name: 'Sara Montagud',
+    login: 'Sara Montagud',
     avatar_url: '/src/images/avatars/afor-avatar.jpg',
     html_url: 'https://www.linkedin.com/in/saramonrod/',
   },
 
   {
-    name: 'Leonardo Salazar',
+    login: 'Leonardo Salazar',
     avatar_url: '/src/images/avatars/leonardo-avatar.jpg',
     html_url: 'https://www.linkedin.com/in/leonardo-salazar-serna/',
   },
